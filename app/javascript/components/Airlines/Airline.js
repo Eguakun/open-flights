59613c2c0a19dfeb52f5bf2950d06532ad251135
Guakanator <eguakunmorris@gmail.com>
2021-11-22 01:30:35 -0600

    Everything is working now except for updating the reviews. The call is being made to the API but the updates are not yet registering

--- app/javascript/components/Airlines/Airline.js
+++ app/javascript/components/Airlines/Airline.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
 import styled from 'styled-components'
+import Rating from '../Rating/Rating'
 
 const Card = styled.div`
   border: 1px solid #efefef;
@@ -45,12 +46,12 @@ const Airline = (props) => {
   return (
     <Card>
       <AirlineLogo>
-      <img src={props.attributes.image_url} alt={props.attributes.name}/>
+        <img src={props.attributes.image_url} alt={props.attributes.name}/>
       </AirlineLogo>
       <AirlineName>{props.attributes.name}</AirlineName>
-      <div className="airline-score">{props.attributes.avg_score}</div>
+      <Rating score={props.attributes.avg_score}/>
       <LinkWrapper>
-        <Link to={'/airlines/${props.attributes.slug}'}>View Airline</Link>
+        <Link to={`/airlines/${props.attributes.slug}`}>View Airline</Link>
       </LinkWrapper>
     </Card>
   )
